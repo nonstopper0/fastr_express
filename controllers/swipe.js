f155528689f36b816f3a65ddbed3c8fc62646d6b
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2020-09-30 11:28:43 -0400

    first route

--- controllers/swipe.js
+++ controllers/swipe.js
@@ -1,4 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/user')
+
+// swipe route - Input: userID, swipeID
+router.post('/', async(reg, res) => {
+    User.findById(req.body.userID, (user) => {
+        console.log(user)
+        console.log(req.body.swipeID, req.body.userID)
+    }
+})
 
 module.exports = router;
